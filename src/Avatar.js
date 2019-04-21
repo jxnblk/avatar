26074ece84830b0a9b0a471895a61feb84613c3a
Brent Jackson <jxnblk@gmail.com>
2019-04-21 16:58:05 -0400

    Edit avatar component

--- src/Avatar.js
+++ src/Avatar.js
@@ -5,16 +5,20 @@ const path = require('./path')
 module.exports = ({
   size = 768,
   pad = true,
-  color = '#000'
+  color = '#000',
+  style,
+  ...props
 }) => h('svg', {
   xmlns: 'http://www.w3.org/2000/svg',
   viewBox: pad ? '-24 -24 176 176' : '0 0 128 128',
   width: size,
   height: size,
   fill: color,
+  ...props,
   style: {
     display: 'block',
-    margin: 0
+    margin: 0,
+    ...style,
   }
 },
   h('path', {
